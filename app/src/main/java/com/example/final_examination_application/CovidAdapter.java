3e6805f48c91c4eefff288bdd833badd7a7a1224
admin <admin>
2020-12-05 22:22:48 +0900

    ID,DATE,AREA,CONTACT_HISTORY add

--- app/src/main/java/com/example/final_examination_application/CovidAdapter.java
+++ app/src/main/java/com/example/final_examination_application/CovidAdapter.java
@@ -50,17 +50,23 @@ public class CovidAdapter extends RecyclerView.Adapter<CovidAdapter.ViewHolder> 
     static class ViewHolder extends RecyclerView.ViewHolder {
         TextView textView;
         TextView textView2;
+        TextView textView3;
+        TextView textView4;
 
         public ViewHolder(View itemView) {
             super(itemView);
 
             textView = itemView.findViewById(R.id.textView);
             textView2 = itemView.findViewById(R.id.textView2);
+            textView3 = itemView.findViewById(R.id.textView3);
+            textView4 = itemView.findViewById(R.id.textView4);
         }
 
         public void setItem(Covid item) {
-            textView.setText("연번 : "+item.CORONA19_ID);
-            textView2.setText(item.CORONA19_AREA);
+            textView.setText("연번 : " + item.CORONA19_ID);
+            textView2.setText("확진일 : " + item.CORONA19_DATE);
+            textView3.setText("지역 : " + item.CORONA19_AREA);
+            textView4.setText("접촉력 : " + item.CORONA19_CONTACT_HISTORY);
         }
 
     }
